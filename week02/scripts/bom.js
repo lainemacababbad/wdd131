35788b57f14357cfa8eedd12565b092df807c629
lainemacababbad <149907223+lainemacababbad@users.noreply.github.com>
2024-09-28 01:53:04 -0600

    Update bom.js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,3 +1,4 @@
+
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
@@ -12,4 +13,21 @@ deleteButton.textContent = '❌';
 
 li.append(deleteButton);
 
-list.append(li);
+button.addEventListener('click', function() {
+  if (input.value !== '') {
+    const newLi = document.createElement('li');
+    const newDeleteButton = document.createElement('button');
+    newLi.textContent = input.value;
+    newDeleteButton.textContent = '❌';
+    newLi.append(newDeleteButton);
+    list.append(newLi);
+    newDeleteButton.addEventListener('click', function () {
+      list.removeChild(newLi);
+      input.focus();
+    });
+    input.value = '';
+  } else {
+    alert('Please enter a chapter!');
+  }
+  input.focus();
+});
