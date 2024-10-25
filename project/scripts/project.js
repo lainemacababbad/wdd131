55c343cbe78eeebcced5f45c27302cff2593546e
lainemacababbad <149907223+lainemacababbad@users.noreply.github.com>
2024-10-25 13:39:22 -0600

    edits 10/25

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -9,3 +9,9 @@ const lastModified = document.lastModified;
 
 const lastModifiedElement = document.getElementById('lastModified');
 lastModifiedElement.textContent = `Last modified: ${lastModified}`;
+
+// hamburger menu
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+}
